e5a0287d72606d4b5af70172cca9e6730d9f01d5
Yusuf <mymmym22@gmail.com>
2020-08-25 11:21:18 +0300

    add oprecorder

--- p404_davinci.mk
+++ p404_davinci.mk
@@ -21,6 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from davinci device
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 $(call inherit-product, vendor/pixelgapps/pixel-gapps.mk)
+$(call inherit-product, vendor/apps/OPScreenRecord/config.mk)
 
 # Inherit some common Wrath stuff.
 $(call inherit-product, vendor/404/configs/common.mk)
